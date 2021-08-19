9c1fdf494bd63cb0264a8dff362015d48feac7b7
steve-128 <70624972+steve-128@users.noreply.github.com>
2021-08-19 14:33:30 +0800

    .

---
+++ ex3.py
@@ -0,0 +1,29 @@
+# it prints text
+print("I will now count my chickens:")
+# it prints text then following a math operation 
+print("Hens", 25 + 30.0 / 6)
+# it prints text then following a math operation
+print("Roosters", 100 - 25.0 * 3 % 4)
+
+# it prints text
+print("Now I will count the eggs:")
+# it prints the anwer of a math operation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6)
+# it prints text
+print("Is it true that 3 + 2 < 5 - 7?")
+# it prints whether the inequality between 2 math operations is true or false
+print(3.0 + 2 < 5.0 - 7)
+# it prints texts then prints whether the inequality between 2 math operations is true or false
+print("What is 3 + 2?", 3.0 + 2)
+# it prints texts then prints whether the inequality between 2 math operations is true or false
+print("What is 5 - 7?", 5.0 - 7)
+# it prints text
+print("Oh, that's why it's False.")
+# it prints text
+print("How about some more.")
+# it prints texts then prints whether the inequality between 2 math operations is true or false
+print("Is it greater?", 5.0 > -2)
+# it prints texts then prints whether the inequality between 2 math operations is true or false
+print("Is it greater or equal?", 5.0 >= -2)
+# it prints texts then prints whether the inequality between 2 math operations is true or false
+print("Is it less or equal?", 5.0 <= -2)
