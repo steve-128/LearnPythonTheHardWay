a114d63384d8ca1da3576f888c8b1114f9b850e2
steve-128 <70624972+steve-128@users.noreply.github.com>
2021-08-20 23:36:30 +0800

    .

---
+++ ex8.py
@@ -0,0 +1,87 @@
+# initialize a variable with for input spaces
+formatter = "{} {} {} {}"
+# put 1,2,3,4 in the input spaces in formatter
+print(formatter.format(1, 2, 3, 4))
+# put "one", "two", "three", "four" in the input spaces in formatter
+print(formatter.format("one", "two", "three", "four"))
+# put True, False, False, True in the input spaces in formatter
+print(formatter.format(True, False, False, True))
+# put 4 formatters in the input spaces in formatter
+print(formatter.format(formatter, formatter, formatter, formatter))
+# put "Try your","Own text here","Maybe a poem","Or a song about fear" in the input spaces in formatter
+print(formatter.format(
+    "Try your",
+    "Own text here",
+    "Maybe a poem",
+    "Or a song about fear"
+))
+# ignore this it's just me testing out stuff
+# basically formatting the formatter and formatting the formatter repeatedly
+print(formatter.format(formatter.format(formatter.format(
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)),
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)), 
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)), 
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter))), 
+formatter.format(formatter.format(
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)),
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)), 
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)), 
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter))),
+formatter.format(formatter.format(
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)),
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)), 
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)), 
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter))),
+formatter.format(formatter.format(
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)),
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)), 
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)), 
+formatter.format(formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter), 
+formatter.format(formatter, formatter, formatter, formatter)))))
