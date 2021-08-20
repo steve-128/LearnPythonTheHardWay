4d3062bf8305f84b5deb76f765bb0a8ba05ac253
steve-128 <70624972+steve-128@users.noreply.github.com>
2021-08-20 23:14:10 +0800

    .

---
+++ ex6.py
@@ -0,0 +1,31 @@
+# initialize 10 to the variable of types of people
+types_of_people = 10
+# initialize a string variable and insert the variable of types_of_people between the variable x
+x = f"There are {types_of_people} types of people."
+# initialize a string variable
+binary = "binary"
+# initialize a string variable
+do_not = "don't"
+# initialize a string variable and insert the variable of do_not and binary between the variable y
+y = f"Those who know {binary} and those who {do_not}."
+# print variable x
+print(x)
+# print variable y
+print(y)
+# print some texts and variable x together
+print(f"I said: {x}")
+# print some texts and variable y together
+print(f"I also said: '{y}'")
+# initialize a boolean variable
+hilarious = False
+# initialize a string variable with a space to insert a variable in
+joke_evaluation = "Isn't that joke so funny?! {}"
+# print the variable joke_evaluation and insert the variable hilarious in the variable space from joke_evaluation 
+print(joke_evaluation.format(hilarious))
+# initialize a string variable
+w = "This is the left side of..."
+# initialize a string variable
+e = "a string with a right side."
+# print the variable w and e together by combining them
+# by adding w and e, it makes a longer string because it combines them
+print(w + e)
---
+++ ex7.py
@@ -0,0 +1,29 @@
+# print texts
+print("Mary had a little lamb.")
+# print texts and insert the word "snow" to the {} place
+print("Its fleece was white as {}.".format('snow'))
+# print texts
+print("And everywhere that Mary went.")
+# print "." by 10 time through multiplying it by 10 
+print("." * 10)  # what'd that do?
+
+# initialize a string variable with a letter(line 11 to 22)
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end.  try removing it to see what happens
+# add end1 to end6 together to form a new string and print it
+# then initialize a variable end by adding the value of ' ' and print it as well
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# add end7 to end12 together to form a new string and print it
+print(end7 + end8 + end9 + end10 + end11 + end12)
