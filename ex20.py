b3b6d43357a22d10497f760740025a1be86be2db
steve-128 <70624972+steve-128@users.noreply.github.com>
2021-08-26 14:05:30 +0800

    .

--- ex20.py
+++ ex20.py
@@ -8,7 +8,7 @@ def print_all(f):
     print(f.read())
 # create a function 
 def rewind(f):
-    ######################################
+    # set the reference point to the very first line with the index of 0
     f.seek(0)
 # create a function 
 def print_a_line(line_count, f):
@@ -27,16 +27,20 @@ rewind(current_file)
 # print texts
 print("Let's print three lines:")
 # Create a variable
+# current_line = 1 aka 1st line 
 current_line = 1
 # call the function print_a_line with the parameter of current line and current file 
 print_a_line(current_line, current_file)
 # Change the variable with current_line 
+# current_line = 2 aka 2nd line
 current_line += 1
 # call the function print_a_line with the parameter of current line and current file 
 print_a_line(current_line, current_file)
 # Change the variable with current_line
+# current_line = 3 aka 3rd line
 current_line += 1
 # call the function print_a_line with the parameter of current line and current file 
 print_a_line(current_line, current_file)
 
+# seek() means to set the reference point  in this case it sets the reference point to the very first line
 # Right argument cheked 
