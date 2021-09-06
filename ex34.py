a0ef05c54fb4c090e89aa76106a3606cfff4b632
steve-128 <70624972+steve-128@users.noreply.github.com>
2021-09-06 22:27:05 +0800

    .

---
+++ ex34.py
@@ -0,0 +1,17 @@
+# call out values from a list
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print("The animal at 1 is", animals[1])
+print("The third animal is",animals[2])
+print("The first animal is", animals[0])
+print("The animal at 3 is", animals[3])
+print("The fifth animal is", animals[4])
+print("The animal at 2 is", animals[2])
+print("The sixth animal is", animals[5])
+print("The animal at 4 is", animals[4])
+
+num = [1,2,3,4,5]
+for x in num:
+    print(x, "is at index",x-1)
+
+# Study drill 2: 
