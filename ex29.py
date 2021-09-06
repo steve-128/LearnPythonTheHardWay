4d8e62811a731c257b76871df63c34da114bc029
steve-128 <70624972+steve-128@users.noreply.github.com>
2021-09-06 12:24:35 +0800

    .

---
+++ ex29.py
@@ -0,0 +1,35 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+        print("Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+
+if people == dogs:
+    print("People are dogs.")
+
+'''
+Study drill:
+1. if does to the code by seeing whether a statement is t or f
+2. So python knows where does the if statement ends
+3. it will not compile
+
+'''
